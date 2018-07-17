fa36892938e34fe5ebe4b92608ccf4e8758c353e
Home Windows <acetaxxxx@gmail.com>
2018-07-17 22:00:38 +0800

    符合LeeetCode格式

--- 118/118.py
+++ 118/118.py
@@ -1,22 +1,27 @@
-def createRow(beforeArray):
-    newArray = [1]
-    if len(beforeArray) >1 :
-        for i in range(1,len(beforeArray)):
-            newArray.append(beforeArray[i-1]+beforeArray[i])
-    newArray.append(1)
-    return newArray
-
-def main(rows):
-    array = [[1]]
-    for item in range(rows-1):
-        array.append(createRow(array[item]))
+class Solution1:
     
-    s = rows*1
-    
-    for item in array :        
-        print("  "*s,item,end=",\n")
-        s-=1
+    def __init__(self):
+       self.__init__()
+        
+    def createRow(beforeArray):
+        newArray = [1]
+        if len(beforeArray) >1 :
+            for i in range(1,len(beforeArray)):
+                newArray.append(beforeArray[i-1]+beforeArray[i])
+        newArray.append(1)
+        return newArray
 
-if __name__ == '__main__':
-    main(4)
+    def main(rows):
+        array = [[1]]
+        for item in range(rows-1):
+            array.append(createRow(array[item]))
+        
+        s = rows*1
+        
+        for item in array :        
+            print("  "*s,item,end=",\n")
+            s-=1
+
+     def generate(self, numRows):
+         main(numRows)
     

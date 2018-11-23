e3f984ff6242c783c38595d3e4fa37e1d9904f85
Home Windows <acetaxxxx@gmail.com>
2018-11-24 00:07:57 +0800

    完成LC937已經可以拆出identifier and Message 以及簡單的一個順序的排序

---
+++ 937. Reorder Log Files/LC937Main.go
@@ -0,0 +1,45 @@
+﻿package main
+
+import (
+	"sort"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func reorderLogFiles(logs []string) []string {
+
+	type log struct {
+		identifier string
+		message    string
+	}
+	tmpResult := make([]log, len(logs))
+	fmt.Println(logs)
+	for i, item := range logs {
+		
+		s := strings.SplitN(item, " ", 2)
+		tmpResult[i] = log{
+			identifier: s[0],
+			message:    s[1],
+		}
+		print("identifier : ",tmpResult[i].identifier, " messages: ",tmpResult[i].message)
+		// fmt.Println(s)
+
+		fmt.Println("")
+	}
+	sort.SliceStable(tmpResult,func(i,j int )bool{
+		return tmpResult[i].message >= tmpResult[j].message
+	})
+	for _, item := range tmpResult {
+		print("Second identifier : ",item.identifier, "  messages: ",item.message)
+		fmt.Println("")
+	}
+	return []string{"g1 act car", "a8 act zoo", "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7"}
+}
+
+func main() {
+
+	s := "abc def"
+	t := "abc bcd"
+	fmt.Printf(strconv.FormatBool(s > t))
+}
---
+++ 937. Reorder Log Files/LC937Main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+
+	"testing"
+)
+
+func Test_reorderLogFiles(t *testing.T) {
+	assert := assert.New(t)
+	type args struct {
+		logs []string
+	}
+	tests := []struct {
+		name   string
+		args   args
+		expects []string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "first",
+			args: args {
+				logs: []string{"a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo"},
+			},
+			expects: []string{"g1 act car", "a8 act zoo", "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(tt.expects, reorderLogFiles(tt.args.logs))
+		})
+	}
+}

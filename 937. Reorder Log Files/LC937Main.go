02bef16738762efb5c459176eabb69f310c1ce60
Home Windows <acetaxxxx@gmail.com>
2018-11-24 18:22:23 +0800

    調整並且使用regexpession

--- 937. Reorder Log Files/LC937Main.go
+++ 937. Reorder Log Files/LC937Main.go
@@ -1,8 +1,9 @@
 ﻿package main
 
 import (
-	"sort"
 	"fmt"
+	"regexp"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -11,30 +12,49 @@ func reorderLogFiles(logs []string) []string {
 
 	type log struct {
 		identifier string
+		isNumber   bool
 		message    string
 	}
 	tmpResult := make([]log, len(logs))
 	fmt.Println(logs)
+	// expression := regexp.QuoteMeta("^\\d+")
+	reg, err1 := regexp.Compile("^[0-9]+ *")
+
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+
 	for i, item := range logs {
-		
+
 		s := strings.SplitN(item, " ", 2)
+
 		tmpResult[i] = log{
 			identifier: s[0],
+			isNumber:   reg.MatchString(s[1]),
 			message:    s[1],
 		}
-		print("identifier : ",tmpResult[i].identifier, " messages: ",tmpResult[i].message)
+		// print("identifier : ", tmpResult[i].identifier, " messages: ", tmpResult[i].message, " isNumber: ", tmpResult[i].isNumber)
 		// fmt.Println(s)
 
-		fmt.Println("")
+		// fmt.Println("")
 	}
-	sort.SliceStable(tmpResult,func(i,j int )bool{
-		return tmpResult[i].message >= tmpResult[j].message
+	sort.SliceStable(tmpResult, func(i, j int) bool {
+		if tmpResult[i].isNumber && tmpResult[j].isNumber {
+			return false
+		}
+		if btou(tmpResult[i].isNumber)+btou(tmpResult[j].isNumber) == 1 {
+			return !tmpResult[i].isNumber
+		}
+
+		return tmpResult[i].message < tmpResult[j].message
 	})
-	for _, item := range tmpResult {
-		print("Second identifier : ",item.identifier, "  messages: ",item.message)
-		fmt.Println("")
+	result := make([]string, len(tmpResult))
+	for i, item := range tmpResult {
+		// print("Second identifier : ", item.identifier, "  messages: ", item.message)
+		// fmt.Println("")
+		result[i] = item.identifier + " " + item.message
 	}
-	return []string{"g1 act car", "a8 act zoo", "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7"}
+	return result
 }
 
 func main() {
@@ -42,4 +62,11 @@ func main() {
 	s := "abc def"
 	t := "abc bcd"
 	fmt.Printf(strconv.FormatBool(s > t))
+}
+
+func btou(b bool) uint8 {
+	if b {
+		return 1
+	}
+	return 0
 }
--- 937. Reorder Log Files/LC937Main_test.go
+++ 937. Reorder Log Files/LC937Main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
-
 	"testing"
 )
 
@@ -12,17 +11,24 @@ func Test_reorderLogFiles(t *testing.T) {
 		logs []string
 	}
 	tests := []struct {
-		name   string
-		args   args
+		name    string
+		args    args
 		expects []string
 	}{
 		// TODO: Add test cases.
 		{
 			name: "first",
-			args: args {
+			args: args{
 				logs: []string{"a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo"},
 			},
 			expects: []string{"g1 act car", "a8 act zoo", "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7"},
+		},
+		{
+			name: "second",
+			args: args{
+				logs: []string{"1 n u", "r 527", "j 893", "6 14", "6 82"},
+			},
+			expects: []string{"1 n u", "r 527", "j 893", "6 14", "6 82"},
 		},
 	}
 	for _, tt := range tests {

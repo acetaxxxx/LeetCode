3cb0966b6779c7e94c85e9a0f015d2fb57d96b26
Home Windows <acetaxxxx@gmail.com>
2018-12-03 23:17:34 +0800

    LC21

---
+++ 21. Merge Two Sorted Lists/LC21Main.go
@@ -0,0 +1,47 @@
+﻿/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+package LeetCode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	result := new(ListNode)
+	iter := result
+	for !(l1.Next == nil || l2.Next == nil) {
+
+		if l1.Val < l2.Val {
+			iter.Val = l1.Val
+			iter.Next = new(ListNode)
+			iter = iter.Next
+			l1 = l1.Next
+
+		} else {
+			iter.Val = l2.Val
+			iter.Next = new(ListNode)
+			iter = iter.Next
+			l2 = l2.Next
+		}
+
+	}
+	if l1.Next == nil {
+		iter.Val = l2.Val
+		iter.Next = l2.Next
+	}
+	if l2.Next == nil {
+		iter.Val = l1.Val
+		iter.Next = l1.Next
+	}
+	return result
+}
+
+func choosen(l1 *ListNode, l2 *ListNode) int {
+	return 1
+}
---
+++ 21. Merge Two Sorted Lists/LC21Main_test.go
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+package LeetCode
+
+import (
+	"github.com/stretchr/testify/assert"
+	
+	"testing"
+)
+
+func Test_mergeTwoLists(t *testing.T) {
+	type args struct {
+		l1 []int
+		l2 []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test",
+			args: args{
+				l1: []int{1, 2, 4},
+				l2: []int{1, 3, 4},
+			},
+			want: []int{1, 1, 2, 3, 4, 4},
+		},
+		{
+			name: "test2",
+			args: args{
+				l1: []int{1,2,4,4,4,4},
+				l2: []int{1,3,4,6,6,64,100},
+			},
+			want: []int{1,1,2,3,4,4,4,4,4,6,6,64,100},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l1 := makeListNode(tt.args.l1)
+			l2 := makeListNode(tt.args.l2)
+			want := makeListNode(tt.want)
+			got := mergeTwoLists(l1,l2)
+			assert.Equal(t, ListToArray(want),ListToArray(got))
+			
+		})
+	}
+}
+func makeListNode(a []int) *ListNode {
+
+	result := &ListNode{}
+	iter := result
+	for i := 0; i < len(a); i++ {
+		iter.Val = a[i]
+		iter.Next = new(ListNode)
+		iter = iter.Next
+	}
+	return result
+}
+func ListToArray(l *ListNode) []int {
+	var r []int
+	for l.Next != nil {
+		r = append(r, l.Val)
+		l = l.Next
+	}
+	return r
+}

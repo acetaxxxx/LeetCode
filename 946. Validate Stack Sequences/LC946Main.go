fe1f0b751478e48fa434a5446ba0eae4ce62cff7
Home Windows <acetaxxxx@gmail.com>
2018-12-05 22:51:27 +0800

    add indexOf function & try complete function

--- 946. Validate Stack Sequences/LC946Main.go
+++ 946. Validate Stack Sequences/LC946Main.go
@@ -1,5 +1,26 @@
 ﻿package LC946
 
 func validateStackSequences(pushed []int, popped []int) bool {
-    return false
+	length := len(pushed)
+	for _, val := range popped {
+		t := indexOf(pushed, val)
+		if t == -1 {
+			return false
+		}
+		if length == 0 {
+			return false
+		}
+		
+	}
+	return false
+}
+
+func indexOf(a []int, t int) int {
+
+	for i, val := range a {
+		if val == t {
+			return i
+		}
+	}
+	return -1
 }
--- 946. Validate Stack Sequences/LC946Main_test.go
+++ 946. Validate Stack Sequences/LC946Main_test.go
@@ -1,6 +1,8 @@
 package LC946
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_validateStackSequences(t *testing.T) {
 	type args struct {
@@ -13,14 +15,49 @@ func Test_validateStackSequences(t *testing.T) {
 		want bool
 	}{
 		// TODO: Add test cases.
-		{
-			
-		},
+		{},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := validateStackSequences(tt.args.pushed, tt.args.popped); got != tt.want {
 				t.Errorf("validateStackSequences() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_indexOf(t *testing.T) {
+	type args struct {
+		a []int
+		t int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases
+		{
+			name: "success",
+			args: args{
+				a: []int{1, 2, 3, 4, 5},
+				t: 3,
+			},
+			want: 2,
+		},
+		{
+			name: "cant find",
+			args: args{
+				a: []int{1, 2, 3, 4, 5},
+				t: 6,
+			},
+			want: -1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := indexOf(tt.args.a, tt.args.t); got != tt.want {
+				t.Errorf("indexOf() = %v, want %v", got, tt.want)
 			}
 		})
 	}

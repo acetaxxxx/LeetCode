f0c6a5a6094c17ef21c010ec216132166bb66fc6
Home Windows <acetaxxxx@gmail.com>
2018-12-10 23:02:55 +0800

    建構大部分結構

---
+++ 542. 01 Matrix/LC542main.go
@@ -0,0 +1,29 @@
+﻿package lc542
+
+var verified map[int]bool
+
+func updateMatrix(matrix [][]int) [][]int {
+	m := len(matrix)
+	n := len(matrix[0])
+	print(verified[1] == false)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if matrix[i][j] == 0 {
+				verified[i*m+j] = true
+			} else if verified[i*m+n] == false {
+				matrix[i][j] = minDistance(1, 2, 3, 4, matrix)
+			}
+		}
+	}
+	// verified := make([][]bool,2,false)
+	return [][]int{{1, 2, 3}, {4, 5, 6}}
+}
+
+func allowed(i, j, m, n int) bool {
+	return i > 0 && j > 0 && i < m && j < n
+}
+
+func minDistance(i, j, m, n int, matrix [][]int) int {
+
+	return 1
+}
---
+++ 542. 01 Matrix/LC542main_test.go
@@ -0,0 +1,34 @@
+package lc542
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_updateMatrix(t *testing.T) {
+	type args struct {
+		matrix [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "first",
+			args: args{
+				matrix:[][]int {{1,2,3,4},{3,4,5,6}},
+			},
+			want: [][]int{{}},
+
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := updateMatrix(tt.args.matrix); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("updateMatrix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

9f8a750a5a4d275567a4270a421a341b798a7ead
Home Windows <acetaxxxx@gmail.com>
2018-12-11 23:58:12 +0800

    修改邏輯使用Stack

--- 542. 01 Matrix/LC542main.go
+++ 542. 01 Matrix/LC542main.go
@@ -2,23 +2,43 @@
 
 import (
 	"math"
+	"github.com/golang-collections/collections/stack"
 )
 
 var verified map[int]bool
+var readyToDo *stack.Stack
+
+type calculateObj struct {
+	I int
+	J int
+	M int 
+	N int	
+}
 
 func updateMatrix(matrix [][]int) [][]int {
 	verified = make(map[int]bool)
+	readyToDo = stack.New()
 	m := len(matrix)
 	n := len(matrix[0])
-	print(verified[1] == false)
+	
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
 			if matrix[i][j] == 0 {
 				verified[i*m+j] = true
-			} else if verified[i*m+j] == false {
-				matrix[i][j] = minDistance(i, j, m, n, matrix)
+			} else if verified[i*m+j] == false {	
+				readyToDo.Push(calculateObj{
+					I:i,
+					J:j,
+					M:m,
+					N:n,					
+				})						
 			}
 		}
+	}
+	for ;readyToDo.Len()>0;{
+		
+		tmp :=  readyToDo.Pop().(calculateObj)  
+		minDistance(tmp.I,tmp.J,tmp.M,tmp.N,matrix)
 	}
 	// verified := make([][]bool,2,false)
 	return matrix
@@ -33,7 +53,10 @@ func min(a, b int) int {
 }
 
 func minDistance(i, j, m, n int, matrix [][]int) int {
-
+	
+	if allowed(i,j,m,n) && verified[i*m+j]==true {
+		return matrix[i][j]
+	}
 	if matrix[i][j] == 0 {
 		verified[i*m+j] = true
 		return 0
@@ -70,5 +93,7 @@ func minDistance(i, j, m, n int, matrix [][]int) int {
 			minResult = min(minResult, 1+matrix[i][j+1])
 		}
 	}
+	verified[i*m+j] = true
+	matrix[i][j] = minResult
 	return minResult
 }

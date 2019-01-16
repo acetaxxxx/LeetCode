4afdc3fae28aaa637730c32ca42e2c77faa30ffc
Home Windows <acetaxxxx@gmail.com>
2019-01-17 00:57:45 +0800

    LC53

---
+++ 53. Maximum Subarray/LC53.go
@@ -0,0 +1,30 @@
+﻿package lc53
+
+import (
+	"math"
+)
+
+func maxSubArray(nums []int) int {
+	length := len(nums)
+	dpmap := make([][]int, length)
+	result := math.MinInt32
+	for i := 0; i < length; i++ {
+		dpmap[i] = make([]int, length)
+		for j := 0; j < length; j++ {
+			if j < i && i-1 >= 0 {
+				dpmap[i][j] = dpmap[i-1][j] + nums[i]
+			} else if i == j {
+				dpmap[i][j] = nums[i]
+			} else {
+				dpmap[i][j] = math.MinInt32
+			}
+			if dpmap[i][j] > result {
+				result = dpmap[i][j]
+			}
+		}
+	}
+
+	return result
+}
+
+
---
+++ 53. Maximum Subarray/LC53_test.go
@@ -0,0 +1,36 @@
+package lc53
+
+import "testing"
+
+func Test_maxSubArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "First",
+			args: args{
+				nums: []int{1, -1, 2, 3, -5},
+			},
+			want: 5,
+		}, {
+			name: "Second",
+			args: args{
+				nums: []int{-2, 1, -3, 4, -1, 2, 1, -5, 4},
+			},
+			want: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxSubArray(tt.args.nums); got != tt.want {
+				t.Errorf("maxSubArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

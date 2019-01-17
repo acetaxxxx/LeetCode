8e4a1cdba4048f93910e8b517c30675f66db0f8a
acetaxxxx@gmail.com <acetaxxxx@gmail.com>
2019-01-17 13:34:05 +0800

    LC11 worst Case

---
+++ 11. Container With Most Water/LC11.go
@@ -0,0 +1,23 @@
+﻿package LC11
+
+import (
+	"math"
+)
+
+func maxArea(height []int) int {
+	length := len(height)
+
+	result := math.MinInt32
+	for i := 0; i < length; i++ {
+		for j := 0; j < length; j++ {
+			if i < j {
+				tmp := int(math.Min(float64(height[i]), float64(height[j])) * float64((j - i)))
+				if tmp > result {
+					result = tmp
+				}
+			}
+		}
+	}
+
+	return result
+}

338f4f2958f8ca566eae82310c91a4254a8f901e
acetaxxxx@gmail.com <acetaxxxx@gmail.com>
2019-01-17 20:08:36 +0800

    fix memory exceed

--- 53. Maximum Subarray/LC53.go
+++ 53. Maximum Subarray/LC53.go
@@ -6,6 +6,28 @@ import (
 
 func maxSubArray(nums []int) int {
 	length := len(nums)
+	dpmap := make([]int, length)
+	result := math.MinInt32
+	for i := 0; i < length; i++ {
+		for j := 0; j < length; j++ {
+			if j < i && i-1 >= 0 {
+				dpmap[j] = dpmap[j] + nums[i]
+			} else if i == j {
+				dpmap[j] = nums[i]
+			} else {
+				dpmap[j] = math.MinInt32
+			}
+			if dpmap[j] > result {
+				result = dpmap[j]
+			}
+		}
+	}
+
+	return result
+}
+
+func maxSubArray1(nums []int) int {
+	length := len(nums)
 	dpmap := make([][]int, length)
 	result := math.MinInt32
 	for i := 0; i < length; i++ {
@@ -26,5 +48,3 @@ func maxSubArray(nums []int) int {
 
 	return result
 }
-
-

425fcbbb76f31557fec9ce70e2c36980f8e7c418
acetaxxxx@gmail.com <acetaxxxx@gmail.com>
2019-01-18 09:48:29 +0800

    finish 11 with O(n)

--- 11. Container With Most Water/LC11.go
+++ 11. Container With Most Water/LC11.go
@@ -7,17 +7,42 @@ import (
 func maxArea(height []int) int {
 	length := len(height)
 
-	result := math.MinInt32
-	for i := 0; i < length; i++ {
-		for j := 0; j < length; j++ {
-			if i < j {
-				tmp := int(math.Min(float64(height[i]), float64(height[j])) * float64((j - i)))
-				if tmp > result {
-					result = tmp
-				}
+	maxIndex := 0
+	result := 0
+	for t := length - 1; t > 0; t-- {
+		var areaL, areaR int
+		areaL = 0
+		areaR = 0
+
+		leftIndex := maxIndex - t
+		if leftIndex >= 0 && leftIndex < length {
+			areaL = minInt(height[maxIndex], height[leftIndex]) * t
+			if height[maxIndex] < height[leftIndex] {
+				maxIndex = leftIndex
 			}
 		}
+
+		rightIndex := maxIndex + t
+		if rightIndex >= 0 && rightIndex < length {
+			areaR = minInt(height[maxIndex], height[rightIndex]) * t
+			if height[maxIndex] < height[rightIndex] {
+				maxIndex = rightIndex
+			}
+		}
+
+		area := maxInt(areaL, areaR)
+		result = maxInt(area, result)
 	}
 
 	return result
+}
+
+func minInt(i, j int) int {
+	r := math.Min(float64(i), float64(j))
+	return int(r)
+}
+
+func maxInt(i, j int) int {
+	r := math.Max(float64(i), float64(j))
+	return int(r)
 }

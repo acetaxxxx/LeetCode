a161f8bff40c9a7550c8e245a8f2fa9e5fb3585d
acetaxxxx@gmail.com <acetaxxxx@gmail.com>
2019-01-18 09:48:54 +0800

    finish 53 with O(n)

--- 53. Maximum Subarray/LC53.go
+++ 53. Maximum Subarray/LC53.go
@@ -1,50 +1,17 @@
 ﻿package lc53
 
-import (
-	"math"
-)
-
 func maxSubArray(nums []int) int {
-	length := len(nums)
-	dpmap := make([]int, length)
-	result := math.MinInt32
-	for i := 0; i < length; i++ {
-		for j := 0; j < length; j++ {
-			if j < i && i-1 >= 0 {
-				dpmap[j] = dpmap[j] + nums[i]
-			} else if i == j {
-				dpmap[j] = nums[i]
-			} else {
-				dpmap[j] = math.MinInt32
-			}
-			if dpmap[j] > result {
-				result = dpmap[j]
-			}
+	result := nums[0]
+	sum := 0
+	for _, val := range nums {
+		if sum > 0 {
+			sum += val
+		} else {
+			sum = val
+		}
+		if sum > result {
+			result = sum
 		}
 	}
-
-	return result
-}
-
-func maxSubArray1(nums []int) int {
-	length := len(nums)
-	dpmap := make([][]int, length)
-	result := math.MinInt32
-	for i := 0; i < length; i++ {
-		dpmap[i] = make([]int, length)
-		for j := 0; j < length; j++ {
-			if j < i && i-1 >= 0 {
-				dpmap[i][j] = dpmap[i-1][j] + nums[i]
-			} else if i == j {
-				dpmap[i][j] = nums[i]
-			} else {
-				dpmap[i][j] = math.MinInt32
-			}
-			if dpmap[i][j] > result {
-				result = dpmap[i][j]
-			}
-		}
-	}
-
 	return result
 }

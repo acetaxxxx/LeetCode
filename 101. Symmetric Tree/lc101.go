8d4098c26f71f033d33ebe33fb4280c819498827
Home Windows <acetaxxxx@gmail.com>
2019-01-30 21:13:47 +0800

    LC101 Finish

---
+++ 101. Symmetric Tree/lc101.go
@@ -0,0 +1,26 @@
+﻿package lc101
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+
+func isSymmetric(root *TreeNode) bool {
+	if root==nil{
+		return false	}
+	return isLevelSymmetric(root.Left,root.Right)
+}
+
+func isLevelSymmetric(left, right *TreeNode) bool {
+	if left != nil && right != nil {
+		if left.Val == right.Val{
+			return isLevelSymmetric(left.Left,right.Right) && isLevelSymmetric(left.Right,right.Left)
+		}
+	}else if left == nil && right == nil{
+		return true
+	}
+	return false
+}
+

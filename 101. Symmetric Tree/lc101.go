3065e9b5d58222d312d51a6d956cf1975ea18218
Home Windows <acetaxxxx@gmail.com>
2019-01-31 20:27:07 +0800

    fix when nil should return true

--- 101. Symmetric Tree/lc101.go
+++ 101. Symmetric Tree/lc101.go
@@ -6,21 +6,20 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-
 func isSymmetric(root *TreeNode) bool {
-	if root==nil{
-		return false	}
-	return isLevelSymmetric(root.Left,root.Right)
+	if root == nil {
+		return true
+	}
+	return isLevelSymmetric(root.Left, root.Right)
 }
 
 func isLevelSymmetric(left, right *TreeNode) bool {
 	if left != nil && right != nil {
-		if left.Val == right.Val{
-			return isLevelSymmetric(left.Left,right.Right) && isLevelSymmetric(left.Right,right.Left)
+		if left.Val == right.Val {
+			return isLevelSymmetric(left.Left, right.Right) && isLevelSymmetric(left.Right, right.Left)
 		}
-	}else if left == nil && right == nil{
+	} else if left == nil && right == nil {
 		return true
 	}
 	return false
 }
-

405e12b99139a17650593df4922085257f5b078f
Hank Yin <hankyin@nine-yi.com>
2020-02-02 13:20:54 +0800

    LC98 first commit

--- 98. Validate Binary Search Tree/LC98.go
+++ 98. Validate Binary Search Tree/LC98.go
@@ -1,1 +1,36 @@
+package lc98
 
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isValidBST(root *TreeNode) bool {
+
+	// if node is null return true
+	if root == nil {
+		return true
+	}
+	// if node left not exist but right exist
+	if root.Left == nil && root.Right != nil {
+		return false
+	}
+
+	if root.Left != nil && root.Val < root.Left.Val {
+		return false
+	}
+
+	if root.Right != nil && root.Val > root.Right.Val {
+		return false
+	}
+	return isValidBST(root.Left) || isValidBST(root.Right)
+}

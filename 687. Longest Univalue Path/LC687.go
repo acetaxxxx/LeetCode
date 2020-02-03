cb4b4ce018dd598e94d2dfe2e4e560084bcd2842
Hank Yin <hankyin@nine-yi.com>
2020-02-03 18:52:56 +0800

    LC 687 stage

--- 687. Longest Univalue Path/LC687.go
+++ 687. Longest Univalue Path/LC687.go
@@ -6,17 +6,16 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-type NewTreeNode struct{
-	Left *NewTreeNode
-	Right *NewTreeNode
-	Val int
-	Length int
-	Parent *NewTreeNode
-}
-
-type Marked struct {
+// Mark
+type Mark struct {
 	Val    int
 	Length int
+}
+
+// newTreeNode
+type NodeResult struct {
+	MaxChild *Mark
+	SelfNode *Mark
 }
 
 func longestUnivaluePath(root *TreeNode) int {
@@ -25,68 +24,18 @@ func longestUnivaluePath(root *TreeNode) int {
 		return 0
 	}
 	
-	newTree := createNewTreeNode(root)
-
-
 }
 
-func createNewTreeNode(root *TreeNode ) *NewTreeNode {
-
-	if root == nil {return nil}
-
-	node := &NewTreeNode{
-		Val: root.Val,
-		Length: 0,
-	}
-
-}
-
-
-func getMarkNode(node *TreeNode, parrentMark *Marked) *Marked {
-	if node == nil {
+func getLongestPath(node *TreeNode)*NodeResult{
+	
+	if  node == nil {
 		return nil
 	}
-	nodeMark := &Marked{
-		Val:    node.Val,
-		Length: 0,
-	}
 
-	if parrentMark != nil && parrentMark.Val == node.Val {
-		nodeMark.Length = parrentMark.Length + 1
-	}
+	leftResult := getLongestPath(node.Left)
+	rightResult := getLongestPath(node.Right)
 
-	leftMax := getMarkNode(node.Left, nodeMark)
-	rightMax := getMarkNode(node.Right, nodeMark)
-
-	return getLargestMark(leftMax, rightMax, nodeMark)
-
-}
-
-func getLargestMark(left *Marked, right *Marked, node *Marked) *Marked {
-	if left == nil && right == nil {
-		return node
-	}
-
-	if left == nil {
-		if right.Length > node.Length {
-			return right
-		}
-		return node
-	}
-	if right == nil {
-		if left.Length > node.Length {
-			return left
-		}
-		return node
-	}
-
-	// node is max
-	if node.Length >= right.Length && node.Length >= left.Length {
-		return node
-	}
-
-	if left.Length > right.Length {
-		return left
-	}
-	return right
+	nodeResult := new(NodeResult)
+	
+	
 }
